d3d3f6aa3300c0a9c0a2707c462136e9c331520c
VidaspVii <vidasp@mail.ru>
2019-08-15 23:43:25 +0300

    Add spec for benchmark rake task reload_json

--- spec/lib/tasks/utils_spec.rb
+++ spec/lib/tasks/utils_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe 'reload_json', :rake do
+describe 'rake reload_json', :rake do
   subject { Rake.application.invoke_task 'reload_json[fixtures/large.json]' }
 
-  it 'should perform under 30 s' do
+  it 'should perform under 60 s' do
     expect{ subject }.to perform_under(60).sec
   end
 end
