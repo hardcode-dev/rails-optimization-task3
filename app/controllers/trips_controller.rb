6d6def8a19c0d87b484a84ab6903ad6915a19be0
Alexey Pasenko <avpasenko@dasreda.ru>
2020-02-14 14:03:21 +0300

    тесты для корректности загружаемых в бд и получаемых из контроллера данных

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,4 +4,12 @@ class TripsController < ApplicationController
     @to = City.find_by_name!(params[:to])
     @trips = Trip.where(from: @from, to: @to).order(:start_time)
   end
+
+  def test
+    @from = City.find_by_name!(params[:from])
+    @to = City.find_by_name!(params[:to])
+    @trips = Trip.where(from: @from, to: @to).order(:start_time)
+
+    render json: @trips
+  end
 end
