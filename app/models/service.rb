72251c724da679716c36c3c4c607b6b9bda5f4c8
Taras Bilous <tbilous@gmail.com>
2020-02-16 23:12:49 +0200

    Task two

--- app/models/service.rb
+++ app/models/service.rb
@@ -14,7 +14,8 @@ class Service < ApplicationRecord
     'Можно не печатать билет',
   ].freeze
 
-  has_and_belongs_to_many :buses, join_table: :buses_services
+  has_many :buses_services
+  has_and_belongs_to_many :buses, through: :buses_services
 
   validates :name, presence: true
   validates :name, inclusion: { in: SERVICES }
