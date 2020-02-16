7d4b25088d39ce31867afc9b036320fc6b9fc683
lokideos <archmage09@gmail.com>
2020-02-17 00:16:19 +0300

    Preload services and buses; change not needed count to size

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class TripsController < ApplicationController
   def index
     @from = City.find_by_name!(params[:from])
     @to = City.find_by_name!(params[:to])
-    @trips = Trip.where(from: @from, to: @to).order(:start_time)
+    @trips = Trip.includes(bus: :services).where(from: @from, to: @to).order(:start_time)
   end
 end
