e5c313adfdb39412a88a352b3e8f4d53c14a2daa
lokideos <archmage09@gmail.com>
2020-02-17 00:34:47 +0300

    Correct table for buses_services

--- db/migrate/20200216165331_create_buses_services.rb
+++ db/migrate/20200216165331_create_buses_services.rb
@@ -1,8 +1,9 @@
 class CreateBusesServices < ActiveRecord::Migration[5.2]
   def change
     create_table :buses_services do |t|
-      t.integer :bus_id
-      t.integer :service_id
+      t.belongs_to :bus, foreign_key: true
+      t.belongs_to :service, foreign_key: true
+
       t.timestamps
     end
   end
