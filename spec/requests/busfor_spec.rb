ecb56bacf7d9c10dc2bbe153132a78ace6c6c611
Egor Razumovsky <rasum239@gmail.com>
2020-02-16 18:07:10 +0300

    шаг 1 - написан тест

---
+++ spec/requests/busfor_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Check buses", :type => :request do
+
+  it "shows direction moscow-samara" do
+
+    Importer.import('fixtures/example.json')
+
+    get URI.escape("/автобусы/Самара/Москва")
+
+
+    #File.write('x1', response.body)
+
+    expect(response.body).to eq(File.read(Rails.root.join('spec/requests/example_json_response.html')))
+  end
+
+end
