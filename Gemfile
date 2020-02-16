7e45f6ce9a40cb659a4862fed5e24541f2d8ec5d
Alexey Pasenko <avpasenko@dasreda.ru>
2020-02-16 12:37:04 +0300

    вторая часть задания

--- Gemfile
+++ Gemfile
@@ -16,6 +16,11 @@ gem 'pghero'
 gem 'pg_query', '>= 0.9.0'
 gem 'ruby-prof'
 
+gem 'rack-mini-profiler', require: false
+gem 'memory_profiler'
+gem 'stackprof', '>= 0.2.9'
+gem 'flamegraph'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/test_performance_utils_spec.rb
+++ spec/test_performance_utils_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 require 'rspec-benchmark'
-# require_relative '../lib/tasks/utils.rake'
 
 RSpec.configure do |config|
   config.include RSpec::Benchmark::Matchers
@@ -10,7 +9,7 @@ describe 'Performance' do
   it 'works under 35 sec' do
     expect {
       system 'rake reload_json[fixtures/large.json]'
-    }.to perform_under(35).sec.warmup(2).times.sample(5).times
+    }.to perform_under(35).sec.warmup(1).times.sample(5).times
   end
 end
 
