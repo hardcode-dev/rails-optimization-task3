02b9034170216e4bf4f4f8f111d3ecdaf7d82e07
{()}JIer <kolomoets.oleg@gmail.com>
2020-02-16 23:06:26 +0200

    Add bullet gem. Fix N+1 problem.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,6 +2,6 @@ class TripsController < ApplicationController
   def index
     @from = params[:from]
     @to = params[:to]
-    @trips = Trip.where(from: @from, to: @to).order(:start_time)
+    @trips = Trip.preload(:bus).where(from: @from, to: @to).order(:start_time)
   end
 end
