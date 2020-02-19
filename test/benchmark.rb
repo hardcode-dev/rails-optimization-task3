f9f031db9a608e6d7116d20793c431360f342fef
Roman Perminov <permidon@me.com>
2020-02-19 09:40:11 -0500

    add case-study

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -2,6 +2,6 @@ require 'benchmark'
 
 puts "Start"
 time = Benchmark.realtime do
-  system 'rake reload_json[fixtures/example.json]'
+  system 'rake reload_json[fixtures/large.json]'
 end
 puts "Finish in #{time.round(2)}"
