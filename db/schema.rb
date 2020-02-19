c16c86de3205fdb5a7af71a6ae1cf2c08fd862a0
Oleg Sachev <cbi444@gmail.com>
2020-02-19 18:03:52 +0300

    Add migration for creating index on 'number' in 'buses' table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_135722) do
+ActiveRecord::Schema.define(version: 2020_02_19_142620) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "buses", force: :cascade do |t|
     t.string "number"
     t.string "model"
+    t.index ["number"], name: "index_buses_on_number"
   end
 
   create_table "buses_services", force: :cascade do |t|
