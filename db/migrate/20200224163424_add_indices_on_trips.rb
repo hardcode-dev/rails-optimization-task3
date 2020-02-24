16877e7c558fa83a685bcd4a57617865f20ed5bc
Oleg Sachev <cbi444@gmail.com>
2020-02-24 20:38:17 +0300

    Add indices on some tables.

---
+++ db/migrate/20200224163424_add_indices_on_trips.rb
@@ -0,0 +1,5 @@
+class AddIndicesOnTrips < ActiveRecord::Migration[5.2]
+  def change
+    add_index :trips, [:from_id, :to_id]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_142620) do
+ActiveRecord::Schema.define(version: 2020_02_24_163842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_142620) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
+    t.index ["name"], name: "index_cities_on_name"
   end
 
   create_table "pghero_query_stats", force: :cascade do |t|
@@ -52,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_142620) do
     t.integer "duration_minutes"
     t.integer "price_cents"
     t.integer "bus_id"
+    t.index ["from_id", "to_id"], name: "index_trips_on_from_id_and_to_id"
   end
 
 end
