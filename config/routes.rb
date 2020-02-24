006719cd1c33318cee94af2bdb14b64ae90c8ff6
Evgeniy Abramov <f1@oren.ru>
2020-02-24 11:44:46 +0500

    Config bullet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount PgHero::Engine, at: "pghero"
 
-  get "/" => "statistics#index"
+  get "/" => "trips#index", defaults: { from: 'Самара', to: 'Москва' }
   get "автобусы/:from/:to" => "trips#index"
 end
