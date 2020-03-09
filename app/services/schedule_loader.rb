735a6ce067f53ace889258578f5698a8d27380f1
Evgeny Gorbachev <djonsilver@yandex.ru>
2020-03-09 18:05:07 +0300

    WIP

--- app/services/schedule_loader.rb
+++ app/services/schedule_loader.rb
@@ -11,19 +11,31 @@ class ScheduleLoader
     @services = {}
     @buses = Set.new
 
-    https : // koshigoe.github.io / postgresql / ruby / 2018 / 10 / 31 / bulk - insert - vs - copy - in -postgres.html
-    @@services = Service.pluck(:name, :id).to_h
+    @trips_command = "copy trips (from_id, to_id, start_time, duration_minutes, price_cents, model, number) from stdin with csv delimiter ';'"
+    @trips_command_raw = "copy trips (from_id, to_id, start_time, duration_minutes, price_cents, model, number) from stdin"
+
+    @bus_command = "copy buses (model, number) from stdin with csv delimiter ';'"
+    @buses_services_command = "copy buses_services (service_id, model, number) from stdin with csv delimiter ';'"
+    @buses_services_command_raw = "copy buses_services (service_id, model, number) from stdin"
+
+
+    @@services = {}
+    Service.pluck(:name, :id).map{|sr| @@services[sr[0].to_sym] = sr[1]}
 
     ActiveRecord::Base.transaction do
-      trips_command = "copy trips (from_id, to_id, start_time, duration_minutes, price_cents, model, number) from stdin with csv delimiter ';'"
-      bus_command = "copy buses (model, number) from stdin with csv delimiter ';'"
-      buses_services_command = "copy buses_services (service_id, model, number) from stdin with csv delimiter ';'"
+
+      time_start = Time.now
 
       ActiveRecord::Base.connection.execute('delete from buses_services;')
-      Trip.delete_all
-      City.delete_all
-      Bus.delete_all
+
       ActiveRecord::Base.connection.execute('delete from buses_services;')
+      ActiveRecord::Base.connection.execute('delete from trips;')
+      ActiveRecord::Base.connection.execute('delete from cities;')
+      ActiveRecord::Base.connection.execute('delete from buses;')
+
+      # Trip.delete_all
+      # City.delete_all
+      # Bus.delete_all
       ActiveRecord::Base.connection.reset_pk_sequence!('cities')
       ActiveRecord::Base.connection.reset_pk_sequence!('buses')
 
@@ -43,20 +55,8 @@ class ScheduleLoader
 
               trip = Oj.load(str)
 
-              unless @buses.include?("#{trip['bus']['number']}-#{trip['bus']['model']}")
-                ActiveRecord::Base.connection.raw_connection.copy_data bus_command do
-                  ActiveRecord::Base.connection.raw_connection.put_copy_data("#{trip['bus']['number']};#{trip['bus']['model']}\n")
-                end
-                ActiveRecord::Base.connection.raw_connection.copy_data buses_services_command do
-                  trip["bus"]["services"].map do |srv|
-                    ActiveRecord::Base.connection.raw_connection.put_copy_data("#{@@services[srv]};#{trip['bus']['number']};#{trip['bus']['model']}\n")
-                  end
-                end
-              end
+              import_trip(trip)
 
-              ActiveRecord::Base.connection.raw_connection.copy_data trips_command do
-                import_trip(trip)
-              end
               str = +''
             end
           elsif nesting >= 1
@@ -65,11 +65,12 @@ class ScheduleLoader
         end
       end
       City.import @cities.map {|cc| ({id: cc[1], name: cc[0]})}
+      end_time = Time.now
+      pp (end_time - time_start)
     end
   end
 
   def self.import_trip(trip)
-    # binding.pry
     from_id = @cities[trip['from']]
     unless from_id
       from_id = @cities.size + 1
@@ -82,8 +83,25 @@ class ScheduleLoader
       @cities[trip['to']] = to_id
     end
 
-    @buses << "#{trip['bus']['number']}-#{trip['bus']['model']}"
+    bus_key = "#{trip['bus']['number']}-#{trip['bus']['model']}"
 
-    ActiveRecord::Base.connection.raw_connection.put_copy_data("#{@cities[trip['from']]};#{@cities[trip['to']]};#{trip['start_time']};#{trip['duration_minutes']};#{trip['price_cents']};#{trip['bus']['number']};#{trip['bus']['model']}\n")
+    bus_number = trip['bus']['number']
+    bus_model = trip['bus']['model']
+
+    unless @buses.include?(bus_key)
+      ActiveRecord::Base.connection.raw_connection.copy_data @bus_command do
+        ActiveRecord::Base.connection.raw_connection.put_copy_data("#{bus_number};#{bus_model}\n")
+      end
+      @buses << bus_key
+      ActiveRecord::Base.connection.raw_connection.copy_data @buses_services_command do
+        trip["bus"]["services"].map do |srv|
+          ActiveRecord::Base.connection.raw_connection.put_copy_data("#{@@services[srv.to_sym]};#{bus_number};#{bus_model}\n")
+        end
+      end
+    end
+
+    ActiveRecord::Base.connection.raw_connection.copy_data @trips_command do
+      ActiveRecord::Base.connection.raw_connection.put_copy_data("#{from_id};#{to_id};#{trip['start_time']};#{trip['duration_minutes']};#{trip['price_cents']};#{bus_number};#{bus_model}\n")
+    end
   end
 end
--- spec/schedule_loader_service_spec.rb
+++ spec/schedule_loader_service_spec.rb
@@ -4,16 +4,16 @@ require "rails_helper"
 
 RSpec.describe ScheduleLoader do
   describe "loading small file" do
-    it { expect { ScheduleLoader.call("fixtures/small.json") }.to perform_under(0.005).sec }
+    it { expect { ScheduleLoader.call("fixtures/small.json") }.to perform_under(2).sec }
   end
 
-  # describe "loading medium file" do
-  #   it {expect { ScheduleLoader.call('fixtures/medium.json') }.to perform_under(60).sec }
-  # end
+  describe "loading medium file" do
+    it {expect { ScheduleLoader.call('fixtures/medium.json') }.to perform_under(60).sec.warmup(1).times.sample(1).times }
+  end
 
-  # describe "loading large file" do
-  #   let(:file_name) { "fixtures/large.json" }
-  #
-  #   it {expect { ScheduleLoader.call('fixtures/large.json') }.to perform_under(60).sec }
-  # end
+  describe "loading large file" do
+    let(:file_name) { "fixtures/large.json" }
+
+    it {expect { ScheduleLoader.call('fixtures/large.json') }.to perform_under(60).sec.warmup(1).times.sample(1).times }
+  end
 end
