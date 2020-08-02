0567c286049ca2e716e8e5e26a7db95888ef5766
m.oleynik <m.oleynik@fun-box.ru>
2020-08-02 21:43:50 +0300

    Добавление индекса в таблицу trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,6 +2,6 @@ class TripsController < ApplicationController
   def index
     @from = City.find_by(name: params[:from])
     @to = City.find_by(name: params[:to])
-    @trips = Trip.includes(bus: [:services]).where(from: @from, to: @to).order(:start_time)
+    @trips = Trip.preload(bus: [:services]).where(from: @from, to: @to).order(:start_time)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_191752) do
+ActiveRecord::Schema.define(version: 2020_08_01_192313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_191752) do
     t.integer "duration_minutes"
     t.integer "price_cents"
     t.integer "bus_id"
+    t.index ["from_id", "to_id"], name: "index_trips_on_from_id_and_to_id"
   end
 
 end
