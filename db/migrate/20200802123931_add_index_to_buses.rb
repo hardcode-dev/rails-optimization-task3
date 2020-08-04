11602514dbc28fd792351ecb7e00cc9723f0587a
SupDex <symeton@gmail.com>
2020-08-04 08:26:53 +0300

    unique index to buses(number, model)

--- db/migrate/20200802123931_add_index_to_buses.rb
+++ db/migrate/20200802123931_add_index_to_buses.rb
@@ -2,6 +2,6 @@ class AddIndexToBuses < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def change
-    add_index(:buses, [:number, :model], algorithm: :concurrently)
+    add_index(:buses, [:number, :model], unique: true, algorithm: :concurrently)
   end
 end
