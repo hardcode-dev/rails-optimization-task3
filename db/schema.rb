8c2634b88b5535b7c76d8e436039a2e31fa9ae42
rabadan731 <rabadan731@yandex.ru>
2020-09-30 02:09:28 +0300

    STEP-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_171920) do
+ActiveRecord::Schema.define(version: 2020_09_29_230507) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "buses", force: :cascade do |t|
@@ -51,6 +52,8 @@ ActiveRecord::Schema.define(version: 2020_09_29_171920) do
     t.integer "duration_minutes"
     t.integer "price_cents"
     t.integer "bus_id"
+    t.index ["from_id", "to_id"], name: "index_trips_on_from_id_and_to_id"
+    t.index ["start_time"], name: "index_trips_on_start_time"
   end
 
 end
