1b85d93e4bd9fc8b02cf161a265ff99dd39b3872
Nikita Ratnikov <nikkie@evrone.com>
2020-10-27 22:31:44 +0300

    add cpu and memory benches

---
+++ spec/benchmarks/reload_json_cpu_benchmark.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+Rails.application.load_tasks
+
+describe 'reload_json_performance' do
+  subject(:task) { Rake::Task['reload_json'].invoke(file) }
+  describe 'Performance' do
+    let(:file) { 'fixtures/small.json' }
+    it 'reports' do
+
+      user_time = Benchmark.realtime do
+        task
+      end
+      puts "finished  in #{user_time}"
+
+      StackProf.run(mode: :wall, out: 'reports/stackprof/cpu/sp.dump', interval: 1200) do
+        task
+      end
+
+      RubyProf.measure_mode = RubyProf::WALL_TIME
+      result = RubyProf.profile do
+        task
+      end
+
+      printer = RubyProf::CallTreePrinter.new(result)
+      printer2 = RubyProf::GraphHtmlPrinter.new(result)
+      printer3 = RubyProf::FlatPrinter.new(result)
+
+      printer.print(path: 'reports/ruby_prof/cpu', profile: 'callgrid')
+      printer2.print(File.open('reports/ruby_prof/cpu/graph.html', 'w+'))
+      printer3.print(STDOUT)
+
+    end
+  end
+end
+
---
+++ spec/benchmarks/reload_json_memory_benchmark.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+Rails.application.load_tasks
+
+describe 'reload_json_performance' do
+  subject(:task) { Rake::Task['reload_json'].invoke(file) }
+  describe 'Performance' do
+    let(:file) { 'fixtures/small.json' }
+    it 'reports' do
+
+      # RubyProf.measure_mode = RubyProf::ALLOCATIONS
+      # rb_result = RubyProf.profile do
+      #   task
+      # end
+      #
+      # rb_printer = RubyProf::FlatPrinter.new(rb_result)
+      # rb_printer.print(File.open("reports/ruby_prof/memory/alloc_flat_#{Time.now.to_i}.txt", 'w+'))
+
+      # RubyProf.measure_mode = RubyProf::MEMORY
+      # rb_result = RubyProf.profile do
+      #   task
+      # end
+      #
+      # rb_printer = RubyProf::CallTreePrinter.new(rb_result)
+      # rb_printer.print(path: 'reports/ruby_prof/memory', profile: 'profile')
+      mp_report = MemoryProfiler.report do
+        task
+      end
+
+      mp_report.pretty_print
+
+      # Benchmark.memory do |x|
+      #   x.report('work') { task }
+      # end
+
+      user_time = Benchmark.realtime do
+        task
+      end
+      expect(user_time).to eq(0)
+    end
+  end
+end
+
