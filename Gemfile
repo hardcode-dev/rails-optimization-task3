f0d92072c15641c5f3d0747c44b7fcc33e1bd366
ordm <ordm67@gmail.com>
2021-10-01 18:26:20 +0300

    problem №1, summary

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'database_cleaner'
   gem 'factory_bot_rails', '~> 6.1'
   gem 'rspec-rails', '~> 4.0'
+  gem 'rspec-benchmark'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/services/data_importer_spec.rb
+++ spec/services/data_importer_spec.rb
@@ -4,32 +4,40 @@ require 'rails_helper'
 
 RSpec.describe DataImporter do
   describe '.call' do
-    before do
-      described_class.call('fixtures/example.json')
+    context 'health check' do
+      before do
+        described_class.call('fixtures/example.json')
+      end
+
+      it 'returns cities count' do
+        expect(City.count).to eq(2)
+      end
+
+      it 'returns cities names' do
+        expect(City.all.map(&:name)).to match_array(%w[Москва Самара])
+      end
+
+      it 'returns buses count' do
+        expect(Bus.count).to eq(1)
+      end
+
+      it 'returns buses' do
+        expect(Bus.all.map { |bus| bus.as_json(only: %i[model number]) }).to match_array([{ 'number' => '123', 'model' => 'Икарус'}])
+      end
+
+      it 'returns services count' do
+        expect(Service.all.map(&:name)).to match_array(Service::SERVICES)
+      end
+
+      it 'returns trips count' do
+        expect(Trip.count).to eq(10)
+      end
     end
 
-    it 'returns cities count' do
-      expect(City.count).to eq(2)
-    end
-
-    it 'returns cities names' do
-      expect(City.all.map(&:name)).to match_array(%w[Москва Самара])
-    end
-
-    it 'returns buses count' do
-      expect(Bus.count).to eq(1)
-    end
-
-    it 'returns buses' do
-      expect(Bus.all.map { |bus| bus.as_json(only: %i[model number]) }).to match_array([{ 'number' => '123', 'model' => 'Икарус'}])
-    end
-
-    it 'returns services count' do
-      expect(Service.all.map(&:name)).to match_array(Service::SERVICES)
-    end
-
-    it 'returns trips count' do
-      expect(Trip.count).to eq(10)
+    context 'performance' do
+      it 'works under 30 seconds' do
+        expect { described_class.call('fixtures/large.json') }.to perform_under(30)
+      end
     end
   end
 end
