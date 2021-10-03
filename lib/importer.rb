c24d08c70bcc337e01203548225bdaa9ff40dc7f
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-10-03 21:59:14 +0300

    Edited profilers

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,38 +1,68 @@
 class Importer
-  class << self
 
-    def perform(filename)
-      json = JSON.parse(File.read(filename))
+  def initialize(filename)
+    @filename = filename || 'fixtures/small.json'
+  end
 
-      ActiveRecord::Base.transaction do
-        City.delete_all
-        Bus.delete_all
-        Service.delete_all
-        Trip.delete_all
-        ActiveRecord::Base.connection.execute('delete from buses_services;')
-
-        json.each do |trip|
-          from = City.find_or_create_by(name: trip['from'])
-          to = City.find_or_create_by(name: trip['to'])
-          services = []
-          trip['bus']['services'].each do |service|
-            s = Service.find_or_create_by(name: service)
-            services << s
-          end
-          bus = Bus.find_or_create_by(number: trip['bus']['number'])
-          bus.update(model: trip['bus']['model'], services: services)
-
-          Trip.create!(
-            from: from,
-            to: to,
-            bus: bus,
-            start_time: trip['start_time'],
-            duration_minutes: trip['duration_minutes'],
-            price_cents: trip['price_cents'],
-            )
-        end
+  def perform(profiler = nil)
+    ActiveRecord::Base.transaction do
+      flush_all
+      case profiler
+      when 'memory_callgrind'
+        memory_callgrind_import
+      when 'time_callgrind'
+        time_callgrind_import
+      else
+        import
       end
     end
-
   end
+
+  private
+
+  def flush_all
+    City.delete_all
+    Bus.delete_all
+    Service.delete_all
+    Trip.delete_all
+    ActiveRecord::Base.connection.execute('delete from buses_services;')
+  end
+
+  def import
+    json = JSON.parse(File.read(@filename))
+
+    json.each do |trip|
+      from = City.find_or_create_by(name: trip['from'])
+      to = City.find_or_create_by(name: trip['to'])
+      services = []
+      trip['bus']['services'].each do |service|
+        s = Service.find_or_create_by(name: service)
+        services << s
+      end
+      bus = Bus.find_or_create_by(number: trip['bus']['number'])
+      bus.update(model: trip['bus']['model'], services: services)
+
+      Trip.create!(
+        from: from,
+        to: to,
+        bus: bus,
+        start_time: trip['start_time'],
+        duration_minutes: trip['duration_minutes'],
+        price_cents: trip['price_cents'],
+        )
+    end
+  end
+
+  def memory_callgrind_import
+    RubyProf.measure_mode = RubyProf::MEMORY
+    result = RubyProf.profile { import }
+    RubyProf::CallTreePrinter.new(result).print(path: 'reports', profile: 'memory_callgrind')
+  end
+
+  def time_callgrind_import
+    RubyProf.measure_mode = RubyProf::WALL_TIME
+    result = RubyProf.profile { import }
+    RubyProf::CallTreePrinter.new(result).print(path: 'reports', profile: 'time_callgrind')
+  end
+
 end
--- lib/tasks/utils.rake
+++ lib/tasks/utils.rake
@@ -1,39 +1,19 @@
 # Загрузка данных из json-файла в БД
 # rake reload_json[fixtures/small.json]
 task :reload_json, [:file_name] => :environment do |_task, args|
-  filename = args.file_name || 'fixtures/example.json'
-  Importer.perform(filename)
+  Importer.new(args.file_name).perform
 end
 
 task :benchmark, [:file_name] => :environment do |_task, args|
-  filename = args.file_name || 'fixtures/small.json'
   Benchmark.bm do |x|
-    x.report { Importer.perform(filename) }
+    x.report { Importer.new(args.file_name).perform }
   end
 end
 
-task :callgrind_time, [:file_name] => :environment do |_task, args|
-  filename = args.file_name || 'fixtures/small.json'
-  RubyProf.measure_mode = RubyProf::WALL_TIME
-
-  result = RubyProf.profile { Importer.perform(filename) }
-
-  printer = RubyProf::CallTreePrinter.new(result)
-  printer.print(path: 'reports', profile: 'time_callgrind')
+task :memory_callgrind, [:file_name] => :environment do |_task, args|
+  Importer.new(args.file_name).perform('memory_callgrind')
 end
 
-task :callgrind_memory, [:file_name] => :environment do |_task, args|
-  filename = args.file_name || 'fixtures/small.json'
-  RubyProf.measure_mode = RubyProf::MEMORY
-
-  result = RubyProf.profile { Importer.perform(filename) }
-
-  printer = RubyProf::CallTreePrinter.new(result)
-  printer.print(path: 'reports', profile: 'memory_callgrind')
-end
-
-task :valgrind, [:file_name] => :environment do |_task, args|
-  filename = args.file_name || 'fixtures/small.json'
-
-  sh "valgrind --tool=massif --massif-out-file=./reports/massif.out rake reload_json[#{filename}]"
+task :time_callgrind, [:file_name] => :environment do |_task, args|
+  Importer.new(args.file_name).perform('time_callgrind')
 end
