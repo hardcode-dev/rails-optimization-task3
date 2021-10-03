9d51eca53441033270d23aeb02d9870ed5177fe2
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-10-04 00:15:50 +0300

    Improved example.json for spec

--- spec/lib/importer_spec.rb
+++ spec/lib/importer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Importer do
   it 'imports data' do
     expect { Importer.new('fixtures/example.json').perform }
-      .to change(Bus, :count).by(1).and change(City, :count).by(2).and change(Trip, :count).by(10)
-      .and change(Service, :count).by(2)
+      .to change(Bus, :count).by(1).and change(City, :count).by(3).and change(Service, :count).by(3)
+      .and change(Trip, :count).by(11)
   end
 end
