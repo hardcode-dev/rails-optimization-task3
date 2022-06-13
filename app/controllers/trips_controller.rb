1385080bb56afe1dc1f1d7f504c70ff9a8bd8573
Efremov Evgenii <e.efremov@3commas.io>
2022-06-13 20:06:25 +0300

    add cached collection

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,9 @@
 class TripsController < ApplicationController
   def index
-    @from = City.find_by_name!(params[:from])
-    @to = City.find_by_name!(params[:to])
-    @trips = Trip.where(from: @from, to: @to).order(:start_time).includes(bus: :services)
+    from = City.find_by_name!(params[:from])
+    to = City.find_by_name!(params[:to])
+    @from_name = from.name
+    @to_name = to.name
+    @trips = Trip.where(from: from, to: to).order(:start_time).includes(bus: :services)
   end
 end
