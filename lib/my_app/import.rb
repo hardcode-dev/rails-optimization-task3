a1117aa4948dfa7fee6338c46a3c714e7fdfe36c
Tab Loid <tabloidmeister@gmail.com>
2023-04-01 18:10:44 +0300

    Extract model creation to separate methods

--- lib/my_app/import.rb
+++ lib/my_app/import.rb
@@ -17,26 +17,53 @@ module MyApp
         ActiveRecord::Base.connection.execute('delete from buses_services;')
 
         json.each do |trip|
-          from = City.find_or_create_by(name: trip['from'])
-          to = City.find_or_create_by(name: trip['to'])
-          services = []
-          trip['bus']['services'].each do |service|
-            s = Service.find_or_create_by(name: service)
-            services << s
-          end
-          bus = Bus.find_or_create_by(number: trip['bus']['number'])
-          bus.update(model: trip['bus']['model'], services: services)
+          from = find_or_create_city(trip['from'])
+          to = find_or_create_city(trip['to'])
+          service_names = trip['bus']['services']
+          services = find_or_create_services(service_names)
+          bus_number = trip['bus']['number']
+          bus_model = trip['bus']['model']
+          bus = find_or_create_bus(bus_number, bus_model, services)
 
-          Trip.create!(
-            from: from,
-            to: to,
-            bus: bus,
-            start_time: trip['start_time'],
-            duration_minutes: trip['duration_minutes'],
-            price_cents: trip['price_cents'],
-            )
+          start_time = trip['start_time']
+          duration = trip['duration_minutes']
+          price = trip['price_cents']
+          add_trip(bus, duration, from, price, start_time, to)
         end
       end
+    end
+
+    private
+
+    def find_or_create_city(city_name)
+      City.find_or_create_by(name: city_name)
+    end
+
+    def find_or_create_services(service_names)
+      service_names.map do |service|
+        find_or_create_service(service)
+      end
+    end
+
+    def find_or_create_service(service)
+      Service.find_or_create_by(name: service)
+    end
+
+    def find_or_create_bus(bus_number, model, services)
+      bus = Bus.find_or_create_by(number: bus_number)
+      bus.update(model: model, services: services)
+      bus
+    end
+
+    def add_trip(bus, duration, from, price, start_time, to)
+      Trip.create!(
+        from: from,
+        to: to,
+        bus: bus,
+        start_time: start_time,
+        duration_minutes: duration,
+        price_cents: price,
+      )
     end
   end
 end
