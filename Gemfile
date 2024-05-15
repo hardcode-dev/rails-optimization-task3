9ecd74e1603bc8fc441d4faf8f61393ad34927b4
potashin <potashin.nikita@gmail.com>
2024-05-15 22:00:32 +0300

    chore: 1st step case b & annotate

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'meta_request'
+  gem 'annotate'
 end
 
 group :test do
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,10 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id   :bigint           not null, primary key
+#  name :string
+#
 class City < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validate :name_has_no_spaces
